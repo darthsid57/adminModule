12a9480e30f9167240e68536a526357e2b79671f
WorkDrive <siddhantanand23@gmail.com>
2018-09-24 18:30:04 +1200

    End of Day

---
+++ .next/server/static/development/pages/user/users.js
@@ -0,0 +1,291 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = require('../../../../ssr-module-cache.js');
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./pages/user/users.js":
+/*!*****************************!*\
+  !*** ./pages/user/users.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_modal__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-modal */ "react-modal");
+/* harmony import */ var react_modal__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_modal__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var semantic_ui_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! semantic-ui-react */ "semantic-ui-react");
+/* harmony import */ var semantic_ui_react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(semantic_ui_react__WEBPACK_IMPORTED_MODULE_2__);
+var _jsxFileName = "/home/workdrive/Desktop/ProjectFolder/adminModule/pages/user/users.js";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+
+
+
+var API = 'http://localhost:4000/users';
+var DEFAULT_QUERY = 'redux';
+
+var Users =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Users, _Component);
+
+  function Users(props) {
+    var _this;
+
+    _classCallCheck(this, Users);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Users).call(this, props));
+    _this.state = {
+      users: []
+    };
+    return _this;
+  }
+
+  _createClass(Users, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      fetch(API + DEFAULT_QUERY).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        return _this2.setState({
+          users: data.users
+        });
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var users = this.state.users;
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 29
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 30
+        },
+        __self: this
+      }, users.map(function (user) {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+          key: user.user_id,
+          __source: {
+            fileName: _jsxFileName,
+            lineNumber: 32
+          },
+          __self: this
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+          href: user.url,
+          __source: {
+            fileName: _jsxFileName,
+            lineNumber: 33
+          },
+          __self: this
+        }, user.title));
+      })));
+    } // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         users: []
+    //     }
+    // }
+    // componentDidMount() {
+    //     let self = this;
+    //     fetch('http://localhost:4000/userss', {
+    //         method: 'GET'
+    //     }).then(function(response) {
+    //         if (response.status >= 400) {
+    //             throw new Error("Bad response from server");
+    //         }
+    //         return response.json();
+    //     }).then(function(data) {
+    //         self.setState({users:data});
+    //         console.log(data)
+    //     }).catch(err => {
+    //     console.log('caught it!',err);
+    //     })
+    // }
+    // renderUsers(){
+    //     const users = this.state.users.map(UserProfiles =>{
+    //         return {
+    //             header: user.user_id,
+    //             description: <a>View User</a>,
+    //             fluid: true
+    //         }
+    //     });
+    //     return <Card.Groupusers users={users}/>;
+    // }users
+    // renderUserID = ({userIusersD, user_id}) => <div key={userID}>{user_id}</div>
+    // renderUserFirstName = users({userFirstName, user_firstname}) => <div key={userFirstName}>{user_firstname}</div>
+    // renderUserLastName = (users{userLastName, user_lastname}) => <div key={userLastName}>{user_lastname}</div>
+
+  }]);
+
+  return Users;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
+
+/* harmony default export */ __webpack_exports__["default"] = (Users);
+
+/***/ }),
+
+/***/ 3:
+/*!***********************************!*\
+  !*** multi ./pages/user/users.js ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! ./pages/user/users.js */"./pages/user/users.js");
+
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ "react-modal":
+/*!******************************!*\
+  !*** external "react-modal" ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("react-modal");
+
+/***/ }),
+
+/***/ "semantic-ui-react":
+/*!************************************!*\
+  !*** external "semantic-ui-react" ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("semantic-ui-react");
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=users.js.map
--- pages/user/users.js
+++ pages/user/users.js
@@ -2,69 +2,81 @@ import React, {Component} from 'react';
 import Modal from 'react-modal';
 import { Button, Card, Image } from 'semantic-ui-react'
 
+const API = 'http://localhost:4000/users';
+const DEFAULT_QUERY = 'redux';
 
 
-export default class Users extends Component {
+class Users extends Component {
 
-    constructor(props) {
-        super(props)
+    constructor(props){
+        super(props);
+
         this.state = {
-            users: []
-        }
+            users: [],
+        };
     }
 
-    componentDidMount() {
-        let self = this;
-        fetch('http://localhost:4000/userss', {
-            method: 'GET'
-        }).then(function(response) {
-            if (response.status >= 400) {
-                throw new Error("Bad response from server");
-            }
-            return response.json();
-        }).then(function(data) {
-            self.setState({users:data});
-            console.log(data)
-        }).catch(err => {
-        console.log('caught it!',err);
-        })
+    componentDidMount(){
+        fetch(API + DEFAULT_QUERY)
+        .then(response => response.json())
+        .then(data => this.setState({ users: data.users }));
     }
 
-    renderUserID = ({userID, user_id}) => <div key={userID}>{user_id}</div>
-    renderUserFirstName = ({userFirstName, user_firstname}) => <div key={userFirstName}>{user_firstname}</div>
+    render(){
+        const {users} = this.state;
 
-    render() {
-      const {users} = this.state;
-      console.log(users);
-        return (
-        <div className="container">
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"></link>
-            <div className="panel panel-default p50 uth-panel">
-                {users.map(this.renderUserID)}
-                {users.map(this.renderUserFirstName)}
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>Member name</th>
-                            <th>Member email</th>
-                            <th>Blood Group</th>
-                            <th>Phone number</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>{users.map(this.renderUserFirstName)}</td>
-                        <td>{users.map(this.renderUserFirstName)}</td>
-                        <td>{users.map(this.renderUserFirstName)}</td>
-                        <td>{users.map(this.renderUserFirstName)}</td>
-                        <td><a>Edit</a>|<a>Delete</a></td>
-                        </tr>
-                    )}
-                    </tbody>
-                </table>
+        return(
+            <div>
+                <ul>
+                    {users.map(user =>
+                        <li key={user.user_id}>
+                            <a href={user.url}>{user.title}</a>
+                        </li>)}
+                </ul>
             </div>
-        </div>
-        );
+        )
     }
+    
+
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         users: []
+    //     }
+    // }
+
+    // componentDidMount() {
+    //     let self = this;
+    //     fetch('http://localhost:4000/userss', {
+    //         method: 'GET'
+    //     }).then(function(response) {
+    //         if (response.status >= 400) {
+    //             throw new Error("Bad response from server");
+    //         }
+    //         return response.json();
+    //     }).then(function(data) {
+    //         self.setState({users:data});
+    //         console.log(data)
+    //     }).catch(err => {
+    //     console.log('caught it!',err);
+    //     })
+    // }
+
+    // renderUsers(){
+    //     const users = this.state.users.map(UserProfiles =>{
+    //         return {
+    //             header: user.user_id,
+    //             description: <a>View User</a>,
+    //             fluid: true
+    //         }
+    //     });
+
+    //     return <Card.Groupusers users={users}/>;
+    // }users
+
+    // renderUserID = ({userIusersD, user_id}) => <div key={userID}>{user_id}</div>
+    // renderUserFirstName = users({userFirstName, user_firstname}) => <div key={userFirstName}>{user_firstname}</div>
+    // renderUserLastName = (users{userLastName, user_lastname}) => <div key={userLastName}>{user_lastname}</div>
 }
+
+export default Users;
